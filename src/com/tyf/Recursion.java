61951f9cec41b9b18195d2d3d1ce83e6151d3d1a
ArunKumar_D <arunkumard1018@gmail.com>
2023-01-11 23:48:04 +0530

    Modified tyf subset sum Recurssion

--- src/com/tyf/Recursion.java
+++ src/com/tyf/Recursion.java
@@ -7,8 +7,8 @@ public class Recursion {
             System.out.print(sum+" ");
             return;
         }
-        SubSetSum(index-1,arr,sum);
         SubSetSum(index-1,arr,sum+arr[index]);
+        SubSetSum(index-1,arr,sum);
 
     }
 
@@ -17,8 +17,8 @@ public class Recursion {
 
     public static void main(String[] args) {
 //        System.out.println("on tyf Recursion main class");
-        int[] arr = {10,1,2,7,6,1,5};
-        SubSetSum(6,arr,0);
+        int[] arr = {1,2,5};
+        SubSetSum(2,arr,0);
     }
 
 }
