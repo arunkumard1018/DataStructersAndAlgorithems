76669fcb8f0202a29a8664c5c90869f2708a7b16
ArunKumar_D <arunkumard1018@gmail.com>
2023-02-20 23:49:17 +0530

    adding the problem solved day_01 Problem_04

--- src/com/tyf/day_01/P_04_SortArrayOfZerosAndOnes.java
+++ src/com/tyf/day_01/P_04_SortArrayOfZerosAndOnes.java
@@ -1,4 +1,62 @@
 package com.tyf.day_01;
-
 public class P_04_SortArrayOfZerosAndOnes {
+    /* Brut Force approach by using Sorting TC: O(nlog(n)) SC:O(1) */
+    public static void solution_01(int[] arr){
+        java.util.Arrays.sort(arr);
+    }
+    /* Optimized approach TC:O(n) */
+    public static void solution_02(int[] arr){
+        int zeros,ones,twos;
+        zeros = ones = twos  = 0;
+        for(int data : arr){
+             if(data == 0)
+                 zeros++;
+             if(data == 1)
+                 ones++;
+             if(data == 2)
+                 twos++;
+            }
+        int i = 0;
+        while (zeros > 0){
+            arr[i] = 0;
+            zeros--; i++;
+        }
+        while (ones > 0){
+            arr[i] = 1;
+            ones--; i++;
+        }
+        while (twos > 0){
+            arr[i] = 2;
+            twos--; i++;
+        }
+    }
+
+    public static void solution_03(int[] arr){
+        int low = 0,mid = 0,heigh = arr.length-1;
+        while (mid<=heigh){
+            switch (arr[mid]){
+                case 0:{
+                    com.utility.Arrays.swap(low,mid,arr);
+                    low++;
+                    mid++;
+                }
+                case 1:{
+                    mid++;
+                }
+                case 2:{
+                    com.utility.Arrays.swap(mid,heigh,arr);
+                    heigh--;
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[] arr = {2,0,2,1,1,0};
+        com.utility.Arrays.display(arr);
+//        solution_01(arr);
+//        solution_02(arr);
+        solution_03(arr);
+        System.out.println("After Execution :");
+        com.utility.Arrays.display(arr);
+    }
 }
--- src/com/utility/Arrays.java
+++ src/com/utility/Arrays.java
@@ -1,8 +1,14 @@
 package com.utility;
 
-public class Arrays{
+public class Arrays<T>{
+
     public static void swap(int a,int b,char[] str){
         char temp = str[a];
+        str[a] = str[b];
+        str[b] = temp;
+    }
+    public static void swap(int a,int b,int[] str){
+        int temp = str[a];
         str[a] = str[b];
         str[b] = temp;
     }
@@ -13,6 +19,11 @@ public class Arrays{
                 System.out.print(data+"\t");
             System.out.println();
         }
-
+    }
+    public static void display(int[] arr){
+        for (int data : arr){
+            System.out.print(data +"\t");
+        }
+        System.out.println();
     }
 }
