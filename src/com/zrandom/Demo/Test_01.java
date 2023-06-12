ec404dfcb387efde72547ce50131e3bed0a2a3a0
ArunKumar_D <arunkumard1018@gmail.com>
2023-06-12 23:22:03 +0530

    adding the problems solved by TAP

---
+++ src/com/zrandom/Demo/Test_01.java
@@ -0,0 +1,7 @@
+package com.zrandom.Demo;
+
+public class Test_01 {
+    public static void main(String[] args) {
+     System.out.print("Hello Arun !");
+    }
+}
---
+++ src/com/zrandom/samples.java
@@ -0,0 +1,6 @@
+package com.zrandom;
+
+public class samples {
+    public int[] un_sorted_array = {12,4,9,18,17,7,5,6};
+    public int[] sorted_array = {2,4,6,8,10};
+}
