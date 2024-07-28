fe66969a961073573d8b54bea1a43c918d40fa55
ArunKumar_D <arunkumard1018@gmail.com>
2024-07-28 23:08:11 +0530

    adding binary search

--- src/com/dsa/P_02_BinarySearch/S_01_BinarySearch.java
+++ src/com/dsa/P_02_BinarySearch/S_01_BinarySearch.java
@@ -1,7 +1,20 @@
 package com.dsa.P_02_BinarySearch;
 
 public class S_01_BinarySearch {
+    public static int[] binarySearch(int[] arr,int key){
+        int low = 0,mid = 0,high = arr.length;
+        while(low<high){
+            mid = (low+high)/2;
+            if(key == arr[mid]) return new int[]{mid};
+            if(key < arr[mid]) high = mid-1;
+            else low = mid+1;
+        }
+        return new int[]{low,mid,high};
+    }
     public static void main(String[] args) {
-
+        int[] arr = {2,4,6,8,10,12};
+        for(int i : binarySearch(arr,1)){
+            System.out.print(i+" ");
+        }
     }
 }
--- src/com/java_concepts_practice_sets/collections/S_08_HashSet.java
+++ src/com/java_concepts_practice_sets/collections/S_08_HashSet.java
@@ -1,4 +1,8 @@
 package com.java_concepts_practice_sets.collections;
 
+
 public class S_08_HashSet {
+    public static void main(String[] args) {
+
+    }
 }
